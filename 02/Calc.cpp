baf0f66bbeae4a6beca82faec623d36746e8242f
Shevchenko Oleg <Opavlyuchenkov@gmail.com>
2019-03-25 17:28:27 +0300

    Finished calculator

--- 02/Calc.cpp
+++ 02/Calc.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include <sstream>
-// #include <vector>
-// #include <algorithm>
 #include <string>
 #include <exception>
 
 using namespace std;
 
 enum Token {
+    Error,
     Number,
     End,
     Plus,
@@ -16,50 +15,53 @@ enum Token {
     Div
 };
 
-struct Result {
+struct Tokenizer {
     int64_t number = 0;
     char prev;
     Token t;
 
-    Result(Token y, char prev = 0, int64_t x = 0) : number(x), prev(prev), t(y) {}
+    Tokenizer(Token y, char prev = 0, int64_t x = 0) : number(x), prev(prev), t(y) {}
 };
 
-Result token(istream* exp) {
+Tokenizer token(istream* exp) {
     char c;
-    while (exp->get(c) && isspace(c));
-
-    switch (c) {
-        case 0:
-            return Result(End);
-        case '-':
-            return Result(Minus, '-');
-        case '+':
-            return Result(Plus, '+');
-        case '*':
-            return Result(Mul, '*');
-        case '/':
-            return Result(Div, '/');
-        default:
-            if (c >= '0' && c <= '9') {
-                return Result(Number, c, c - '0');
-            }
+    while(exp->get(c)) {
+        switch (c) {
+            case ' ':
+                continue;
+            case '-':
+                return Tokenizer(Minus, '-');
+            case '+':
+                return Tokenizer(Plus, '+');
+            case '*':
+                return Tokenizer(Mul, '*');
+            case '/':
+                return Tokenizer(Div, '/');
+            default:
+                if (c >= '0' && c <= '9')
+                    return Tokenizer(Number, c, c - '0');
+                else
+                    return Tokenizer(Error, c);
+        }
     }
+    return Tokenizer(End);
+
 }
 
 int64_t prim(istream* exp) {
-    Result res = token(exp);
+    Tokenizer res = token(exp);
 
     switch (res.t) {
         case Minus:
             return -prim(exp);
         case Plus:
             return prim(exp);
-        case End:
-            throw ("something");
         case Number:
             return res.number;
-        default:
-            throw ("something");
+        case Error:
+            throw invalid_argument("invalid expression");
+        case End:
+            throw logic_error("End expression");
     }
 }
 
@@ -67,15 +69,22 @@ int64_t term(istream* exp) {
     int64_t left = prim(exp);
 
     while (true) {
-        Result res = token(exp);
+        Tokenizer res = token(exp);
 
         switch(res.t) {
             case Mul:
                 left *= prim(exp);
                 break;
-            case Div:
-                left /= prim(exp);
+            case Div: {
+                int64_t d = prim(exp);
+                if (d == 0)
+                    throw runtime_error("Divide by Zero");
+                else
+                    left /= d;
                 break;
+            }
+            case Error:
+                throw invalid_argument("invalid expression");
             default:
                 exp->putback(res.prev);
                 return left;
@@ -87,7 +96,7 @@ int64_t expr(istream* exp) {
     int64_t left = term(exp);
 
     while (true) {
-        Result res = token(exp);
+        Tokenizer res = token(exp);
 
         switch (res.t) {
             case Plus:
@@ -96,6 +105,8 @@ int64_t expr(istream* exp) {
             case Minus:
                 left -= term(exp);
                 break;
+            case Error:
+                throw invalid_argument("invalid expression");
             default:
                 return left;
         }
@@ -103,8 +114,22 @@ int64_t expr(istream* exp) {
 }
 
 int main(int argc, char* argv[]) {
-    std::istream* text = nullptr;
-    text = new std::istringstream(argv[1]);
-    cout << expr(text) << endl;
+    if (argc != 2) {
+        cout << "error" << endl;
+        return 1;
+    }
+    auto text = new std::istringstream(argv[1]);
+    try{
+        cout << expr(text) << endl;
+    }
+
+    catch (runtime_error& e) {
+        cout << "error" << endl;
+        return 1;
+    }
+    catch  (logic_error& e) {
+        cout << "error" << endl;
+        return 1;
+    }
     return 0;
 }
